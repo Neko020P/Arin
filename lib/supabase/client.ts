e55d790d362b3fecf6dc629c690025b9f918b568
Chomphunuch <chomphunuch1346@gmail.com>
2026-07-11 01:11:42 +0700

    dev12

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,16 @@
-import { createBrowserClient } from '@supabase/ssr'
+//client.ts
+import { createClient as createBrowserClient } from '@supabase/supabase-js'
 
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        detectSessionInUrl: true,
+        autoRefreshToken: true,
+      },
+    }
   )
 }
